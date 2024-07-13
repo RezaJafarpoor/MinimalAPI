ee75c84598f451b97db3a21fb1613b62506df2bd
reza <rezajafarpoor222@gmail.com>
2024-07-13 21:39:03 +0330

    Initial Commit

---
+++ MinimalAPI/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPI.Entities;
+
+namespace MinimalAPI.Persistence;
+
+public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Book> Books{ get; set; }
+}
